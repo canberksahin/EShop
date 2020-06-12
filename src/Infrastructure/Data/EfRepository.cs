c0bdad0ab6958e7ef9ad195d85cd95757c7703bb
Canberk Sahin <canberksahin20@hotmail.com>
2020-06-12 22:01:31 +0300

    IAsyncRepository & EfRepository

---
+++ src/Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class EfRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        private readonly ShopContext _dbcontext;
+
+        public EfRepository(ShopContext context)
+        {
+            _dbcontext = context;
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbcontext.Set<T>().ToListAsync();
+        }
+    }
+}
